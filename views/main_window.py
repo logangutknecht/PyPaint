ccdfbe082eec177eaff1b3919b47a718939b1c3e
Logan Gutknecht <logan.gutknecht@xplore.com>
2024-05-22 14:33:25 -0700

    Added 8bit pixel art for buttons

--- views/main_window.py
+++ views/main_window.py
@@ -1,19 +1,43 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 class MainWindow(tk.Tk):
     def __init__(self, controller):
         super().__init__()
-        self.title("MS Paint Clone")
+        self.title("PyPaint")
         self.controller = controller
 
+        self.toolbar = tk.Frame(self)
+        self.toolbar.pack(side=tk.TOP, fill=tk.X)
+
+        self.pen_icon = self.load_icon("pen.png")
+        self.pen_button = tk.Button(self.toolbar, image=self.pen_icon, command=lambda: self.controller.on_tool_select("pen"))
+        self.pen_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.pen_button.bind("<Enter>", lambda event: self.show_description("Pen"))
+        self.pen_button.bind("<Leave>", self.hide_description)
+
+        self.eraser_icon = self.load_icon("eraser.png")
+        self.eraser_button = tk.Button(self.toolbar, image=self.eraser_icon, command=lambda: self.controller.on_tool_select("eraser"))
+        self.eraser_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.eraser_button.bind("<Enter>", lambda event: self.show_description("Eraser"))
+        self.eraser_button.bind("<Leave>", self.hide_description)
+
+        self.description_label = tk.Label(self.toolbar, text="", fg="black", bg="white", padx=5, pady=2)
+
         self.drawing_canvas = tk.Canvas(self, width=800, height=600, bg="white")
-        self.drawing_canvas.pack()
+        self.drawing_canvas.pack(fill=tk.BOTH, expand=True)
 
-        self.pen_button = tk.Button(self, text="Pen", command=lambda: self.controller.on_tool_select("pen"))
-        self.pen_button.pack(side=tk.LEFT)
+    def load_icon(self, filename):
+        icon = Image.open(filename)
+        icon = icon.resize((24, 24), Image.LANCZOS)
+        return ImageTk.PhotoImage(icon)
 
-        self.eraser_button = tk.Button(self, text="Eraser", command=lambda: self.controller.on_tool_select("eraser"))
-        self.eraser_button.pack(side=tk.LEFT)
+    def show_description(self, text):
+        self.description_label.config(text=text)
+        self.description_label.pack(side=tk.LEFT)
+
+    def hide_description(self, event):
+        self.description_label.pack_forget()
 
     def bind_mouse_events(self, on_mouse_press, on_mouse_drag, on_mouse_release):
         self.drawing_canvas.bind("<Button-1>", on_mouse_press)
